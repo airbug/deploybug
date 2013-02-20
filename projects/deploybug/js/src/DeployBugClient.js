4d5fe0bf21f39fc10f1a76156cb9c80bc81df18d
Sung Choi <m.sung.choi@gmail.com>
2013-02-20 09:36:08 -0800

    DeployBug. DeployBugClient. Adds 'update', 'start', and 'stop' methods

--- projects/deploybug/js/src/DeployBugClient.js
+++ projects/deploybug/js/src/DeployBugClient.js
@@ -79,9 +79,31 @@ DeployBugClient.registerPackage = function(descriptionJSON, serverHostname, serv
     
 };
 
-// DeployBugClient.updatePackage = function(key, descriptionJSON, serverHostname, serverPort, callback){
-//     
-// };
+DeployBugClient.updatePackage = function(key, descriptionJSON, serverHostname, serverPort){
+    var data = JSON.stringify(descriptionJSON);
+    var options = {
+      hostname: serverHostname,
+      port: serverPort,
+      path: '/deploybug/packages/' + key + '/update',
+      method: 'POST',
+      headers: {'Accept': 'application/json', 'Content-Type': 'application/json'}
+    };
+    var req = http.request(options, function(res){  // DRY up this req object
+        console.log('Response from DeployBugServer:');
+        console.log('STATUS: ' + res.statusCode);
+        console.log('HEADERS: ' + JSON.stringify(res.headers));
+    }).on('error', function(error) {
+        console.log('problem with request: ' + error.message);
+    }).on('response', function(res) {
+        res.setEncoding('utf8');
+        res.on('data', function (chunk) {
+            console.log('BODY: ' + chunk);
+        });
+    });
+        
+    req.write(data, 'utf8');
+    req.end();
+};
 
 DeployBugClient.deployPackage = function(key, serverHostname, serverPort) {
     var options = {
@@ -108,12 +130,52 @@ DeployBugClient.deployPackage = function(key, serverHostname, serverPort) {
     
 };
 
-DeployBugClient.startPackage = function(key, callback) {
-    
+DeployBugClient.startPackage = function(key, serverHostname, serverPort) {
+    var options = {
+      hostname: serverHostname,
+      port: serverPort,
+      path: '/deploybug/packages/' + key + '/start',
+      method: 'POST',
+      headers: {}
+    };
+    var req = http.request(options, function(res){
+        console.log('Response from DeployBugServer:');
+        console.log('STATUS: ' + res.statusCode);
+        console.log('HEADERS: ' + JSON.stringify(res.headers));
+    }).on('response', function(res) {
+        res.setEncoding('utf8');
+        res.on('data', function (chunk) {
+            console.log('BODY: ' + chunk);
+        });
+    }).on('error', function(error) {
+        console.log('problem with request: ' + error.message);
+    });
+
+    req.end();
 };
 
-DeployBugClient.stopPackage = function(key, callback) {
-    
+DeployBugClient.stopPackage = function(key, serverHostname, serverPort) {
+    var options = {
+      hostname: serverHostname,
+      port: serverPort,
+      path: '/deploybug/packages/' + key + '/stop',
+      method: 'POST',
+      headers: {}
+    };
+    var req = http.request(options, function(res){
+        console.log('Response from DeployBugServer:');
+        console.log('STATUS: ' + res.statusCode);
+        console.log('HEADERS: ' + JSON.stringify(res.headers));
+    }).on('response', function(res) {
+        res.setEncoding('utf8');
+        res.on('data', function (chunk) {
+            console.log('BODY: ' + chunk);
+        });
+    }).on('error', function(error) {
+        console.log('problem with request: ' + error.message);
+    });
+
+    req.end();
 };
 
 
