beac101609b373a572b83e907dac13fc7b95bb51
Sung Choi <m.sung.choi@gmail.com>
2013-02-19 16:18:35 -0800

    DeployBug. deploybugserver-start.js. Removes callback function from deployBugServer.start() method call

--- projects/deploybug/js/scripts/deploybugserver-start.js
+++ projects/deploybug/js/scripts/deploybugserver-start.js
@@ -26,12 +26,4 @@ var deployBugServer = bugpack.require('deploybug.DeployBugServer');
 // var deployBugServer = new DeployBugServer();
 // or var deployBugServer = bugpack.require('deploybug.DeployBugServer');
 
-deployBugServer.start(function(error) {
-    if (error) {
-        console.log(error);
-        console.log(error.stack);
-        process.exit(1);
-    } else {
-        console.log("DeployBug server successfully started");
-    }
-});
+deployBugServer.start();
