75e1a49804eab7667e75eae609ec6cef7ef13929
Sung Choi <m.sung.choi@gmail.com>
2013-02-22 10:52:57 -0800

    DeployBug. DeployBugClient. DRYs up similar http requests. Updates closure annotations. Adds restartPackage method.

--- projects/deploybug/js/src/DeployBugClient.js
+++ projects/deploybug/js/src/DeployBugClient.js
@@ -29,7 +29,8 @@ var DeployBugClient = {};
 // Private Static Variables
 //-------------------------------------------------------------------------------
 
-DeployBugClient.server = null;
+DeployBugClient.serverName = null;
+DeployBugClient.serverPort = null;
 
 //-------------------------------------------------------------------------------
 // Public Static Methods
@@ -39,14 +40,10 @@ DeployBugClient.server = null;
  * @param {{
  *  packageURL: string,
  *  packageType: string,
- *  deployScript: (string | Path),
  *  startScript: (string | Path),
- *  stopScript: (string | Path),
- *  restartScript: (string | Path)
  * }} descriptionJSON,
  * @param {string} serverHostname
  * @param {number} serverPort
- *
  */
 DeployBugClient.registerPackage = function(descriptionJSON, serverHostname, serverPort) {
     var data = JSON.stringify(descriptionJSON);
@@ -57,24 +54,24 @@ DeployBugClient.registerPackage = function(descriptionJSON, serverHostname, serv
       method: 'POST',
       headers: {'Accept': 'application/json', 'Content-Type': 'application/json'}
     };
-    var req = http.request(options, function(res){  // DRY up this req object
-        console.log('Response from DeployBugServer:');
-        console.log('STATUS: ' + res.statusCode);
-        console.log('HEADERS: ' + JSON.stringify(res.headers));
-    }).on('error', function(error) {
-        console.log('problem with request: ' + error.message);
-    }).on('response', function(res) {
-        res.setEncoding('utf8');
-        res.on('data', function (chunk) {
-            console.log('BODY: ' + chunk);
-        });
-    });
-        
-    req.write(data, 'utf8');
-    req.end();
+    
+    HTTPRequest.send(options, data);
     
 };
 
+/**
+ * @param {string} key
+ * @param {{
+ *  key: string,
+ *  hostname: string,
+ *  port: number,
+ *  packageURL: string,
+ *  packageType: string,
+ *  startScript: (string | Path),
+ * }} descriptionJSON
+ * @param {string} serverHostname
+ * @param {number} serverPort
+ */
 DeployBugClient.updatePackage = function(key, descriptionJSON, serverHostname, serverPort){
     var data = JSON.stringify(descriptionJSON);
     var options = {
@@ -84,23 +81,15 @@ DeployBugClient.updatePackage = function(key, descriptionJSON, serverHostname, s
       method: 'PUT',
       headers: {'Accept': 'application/json', 'Content-Type': 'application/json'}
     };
-    var req = http.request(options, function(res){  // DRY up this req object
-        console.log('Response from DeployBugServer:');
-        console.log('STATUS: ' + res.statusCode);
-        console.log('HEADERS: ' + JSON.stringify(res.headers));
-    }).on('error', function(error) {
-        console.log('problem with request: ' + error.message);
-    }).on('response', function(res) {
-        res.setEncoding('utf8');
-        res.on('data', function (chunk) {
-            console.log('BODY: ' + chunk);
-        });
-    });
-        
-    req.write(data, 'utf8');
-    req.end();
+    
+    HTTPRequest.send(options, data);
 };
 
+/**
+ * @param {string} key
+ * @param {string} serverHostname
+ * @param {number} serverPort
+ */
 DeployBugClient.deployPackage = function(key, serverHostname, serverPort) {
     var options = {
       hostname: serverHostname,
@@ -109,23 +98,16 @@ DeployBugClient.deployPackage = function(key, serverHostname, serverPort) {
       method: 'POST',
       headers: {}
     };
-    var req = http.request(options, function(res){   // DRY up this req object
-        console.log('Response from DeployBugServer:');
-        console.log('STATUS: ' + res.statusCode);
-        console.log('HEADERS: ' + JSON.stringify(res.headers));
-    }).on('response', function(res) {
-        res.setEncoding('utf8');
-        res.on('data', function (chunk) {
-            console.log('BODY: ' + chunk);
-        });
-    }).on('error', function(error) {
-        console.log('problem with request: ' + error.message);
-    });
-
-    req.end();
+    
+    HTTPRequest.send(options);
     
 };
 
+/**
+ * @param {string} key
+ * @param {string} serverHostname
+ * @param {number} serverPort
+ */
 DeployBugClient.startPackage = function(key, serverHostname, serverPort) {
     var options = {
       hostname: serverHostname,
@@ -136,22 +118,15 @@ DeployBugClient.startPackage = function(key, serverHostname, serverPort) {
       method: 'PUT',
       headers: {}
     };
-    var req = http.request(options, function(res){
-        console.log('Response from DeployBugServer:');
-        console.log('STATUS: ' + res.statusCode);
-        console.log('HEADERS: ' + JSON.stringify(res.headers));
-    }).on('response', function(res) {
-        res.setEncoding('utf8');
-        res.on('data', function (chunk) {
-            console.log('BODY: ' + chunk);
-        });
-    }).on('error', function(error) {
-        console.log('problem with request: ' + error.message);
-    });
-
-    req.end();
+    
+    HTTPRequest.send(options);
 };
 
+/**
+ * @param {string} key,
+ * @param {string} serverHostname,
+ * @param {number} serverPort
+ */
 DeployBugClient.stopPackage = function(key, serverHostname, serverPort) {
     var options = {
       hostname: serverHostname,
@@ -160,28 +135,61 @@ DeployBugClient.stopPackage = function(key, serverHostname, serverPort) {
       method: 'PUT',
       headers: {}
     };
-    var req = http.request(options, function(res){
-        console.log('Response from DeployBugServer:');
-        console.log('STATUS: ' + res.statusCode);
-        console.log('HEADERS: ' + JSON.stringify(res.headers));
-    }).on('response', function(res) {
-        res.setEncoding('utf8');
-        res.on('data', function (chunk) {
-            console.log('BODY: ' + chunk);
-        });
-    }).on('error', function(error) {
-        console.log('problem with request: ' + error.message);
-    });
-
-    req.end();
+    
+    HTTPRequest.send(options);
 };
 
+/**
+ * @param {string} key,
+ * @param {string} serverHostname,
+ * @param {number} serverPort
+ */
+DeployBugClient.restartPackage = function(key, serverHostname, serverPort) {
+    var options = {
+      hostname: serverHostname,
+      port: serverPort,
+      path: '/deploybug/packages/' + key + '/restart',
+      method: 'PUT',
+      headers: {}
+    };
+    
+    HTTPRequest.send(options);
+};
 
 //-------------------------------------------------------------------------------
 // Private Methods
 //-------------------------------------------------------------------------------
 
 
+var HTTPRequest = {
+    /**
+     * @param {{
+     *  hostname: string,
+     *  port: number,
+     *  path: string,
+     *  method: string,
+     *  headers: {},
+     * }} options
+     * @param {*} data
+     */
+    send: function(options, data){
+        var req = http.request(options, function(res){
+            console.log('Response from DeployBugServer:');
+            console.log('STATUS: ' + res.statusCode);
+            console.log('HEADERS: ' + JSON.stringify(res.headers));
+        }).on('response', function(res) {
+            res.setEncoding('utf8');
+            res.on('data', function (chunk) {
+                console.log('BODY: ' + chunk);
+            });
+        }).on('error', function(error) {
+            console.log('problem with request: ' + error.message);
+        });
+
+        req.write(data, 'utf8');
+        req.end();
+    }
+}; 
 
 //-------------------------------------------------------------------------------
 // Exports
